0c26da5b83f198eb3600767797803a24887636fd
droid-101 <shanepinto18@yahoo.com>
2020-11-02 16:56:25 -0500

    Comments
    Comments have been added to code.

--- robot.c
+++ robot.c
@@ -1,5 +1,14 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Program: robot.c
+Authour: Shane Pinto
+Date: Monday, November 2, 2020
+Description: Runs a routine to make
+a robot complete a playing field.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 #include <pic.h>
 
+// HARDWARE INPUT AND OUTPUT PIN DESIGNATIONS //
 #define LEFT_SENSOR_CHANNEL  2
 #define RIGHT_SENSOR_CHANNEL 1
 
@@ -8,20 +17,26 @@
 #define LEFT_MOTOR_FORWARD   RB7
 #define LEFT_MOTOR_REVERSE   RB6
 
+// VALUES DEPENDENT ON BATTERY CHARGE AND SPEED //
 #define SENSOR_THRESHOLD     50
 #define MAX_WIDTH            15
 #define ENTER_EXIT_DELAY     750000
 
 __CONFIG( FOSC_INTRCIO & WDTE_OFF & PWRTE_OFF & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_OFF & IESO_OFF & FCMEN_OFF );
 
-enum Sensor {RIGHT_SENSOR, LEFT_SENSOR};
-enum Direction {RIGHT, LEFT};
+enum Sensor {RIGHT_SENSOR, LEFT_SENSOR}; // Arguments that will determine which sensor is read.
+enum Direction {RIGHT, LEFT};            // Arguments that will determine which direction the robot is travelling around the field.
 
+// INITIALIZATION FUNCTIONS //
 void init_hardware(void);
+void reset_barcode_width(void);
+
+// SENSOR COMPUTING FUNCTIONS //
 int get_sensor(enum Sensor side);
 char black(int reading);
 char white(int reading);
-void reset_barcode_width(void);
+
+// MOVEMENT FUNCTIONS //
 void stop(void);
 void forward(void);
 void reverse(void);
@@ -34,6 +49,8 @@ void reverse_left(void);
 void test(void);
 void drive_right(void);
 void drive_left(void);
+
+// ROUTINE FUNCTIONS //
 void count_marker(enum Direction direction);
 void start(void);
 void leave(enum Direction direction);
@@ -44,33 +61,36 @@ void go_to_destination(unsigned char destination);
 void dock(enum Direction direction);
 void go_home(unsigned char destination);
 
+// VARIABLE DECLARTIONS //
+unsigned int left_sensor = 0;                    // Stores the value of the last reading from the left light sensor.
+unsigned int right_sensor = 0;                   // Stores the value of the last reading from the right light sensor.
+signed char marker_count = 0;                    // Keeps track of how many markers or sections have been passed.
+signed char markers_to_destination = 0;          // Determines how many markers the robot must pass to reach its destination.
+signed char barcode = 0;                         // Keeps track of how many barcode lines have been read.
+signed char width = 0;                           // Stores the width of a line before it is sent to the array for storage.
+unsigned char destination = 0;                   // Stores the destination in which the robot must travel to.
+signed char barcode_width [5] = {0, 0, 0, 0, 0}; // Stores the widths of the lines read.
 
-unsigned int left_sensor = 0;
-unsigned int right_sensor = 0;
-signed char marker_count = 0;
-signed char markers_to_destination = 0;
-signed char barcode = 0;
-signed char width = 0;
-unsigned char destination = 0;
-signed char barcode_width [5] = {0, 0, 0, 0, 0};
+
+// ========================= MAIN ========================= //
 
 void main(void)
 {
-    OSCCONbits.IRCF = 0b111;
-    OSCCONbits.SCS = 1;
+    OSCCONbits.IRCF = 0b111; // Set clock speed to 8MHz.
+    OSCCONbits.SCS = 1;      // Use internal oscillator for system clock.
 
     init_hardware();
 
-    TRISA = 0b00110110;
+    TRISA = 0b00110110; // Set pins AN1 and AN2 on the A register to inputs for the sensors.
 
-    ANSEL = 0b00000110;
-
-    ADCON0 = 0b00000001;
+    ANSEL = 0b00000110;  // Set pins AN1 and AN2 to analogue inputs.
+    ADCON0 = 0b00000001; // Turn on the ADC.
 
     stop();
 
     while((RA5 == 0))
     {
+        // ======= PROGRAM INITIALIZATION =======//
         left_sensor = 0;
         right_sensor = 0;
         marker_count = 0;
@@ -105,6 +125,16 @@ void main(void)
 
 
 
+// ========================= METHODS ========================= //
+
+
+/* ================================
+Function: go_home
+Paramaters: unsigned char destination
+return type: none
+Description: Drives robot back to
+start from current location.
+================================ */
 void go_home(unsigned char destination)
 {
     switch (destination)
@@ -198,6 +228,13 @@ void go_home(unsigned char destination)
     stop();
 }
 
+/* ================================
+Function: dock
+Paramaters: enum Direction direction
+return type: none
+Description: Instructions for the
+robot to turn into the destination.
+================================ */
 void dock(enum Direction direction)
 {
     if (direction == RIGHT)
@@ -234,6 +271,14 @@ void dock(enum Direction direction)
     }
 }
 
+/* ================================
+Function: go_to_destination
+Paramaters: unsigned char destination
+return type: none
+Description: Robot will drive to
+the destination read and will dock
+into the section.
+================================ */
 void go_to_destination(unsigned char destination)
 {
     markers_to_destination = 0;
@@ -317,6 +362,14 @@ void go_to_destination(unsigned char destination)
     _delay(2000000);
 }
 
+/* ================================
+Function: scan_barcode
+Paramaters: none
+return type: none
+Description: Moves robot over
+barcode, calculates the correct
+destination, then exits the section.
+================================ */
 void scan_barcode(void)
 {
     while (barcode_width[1] < MAX_WIDTH && barcode_width[2] < MAX_WIDTH && barcode_width[3] < MAX_WIDTH && barcode_width[4] < MAX_WIDTH)
@@ -334,7 +387,6 @@ void scan_barcode(void)
             }
 
             barcode++;
-            PORTC = barcode;
         }
     }
 
@@ -354,15 +406,22 @@ void scan_barcode(void)
 
 	for (int i = 0; i < destination + 2; i++)
     {
-        while (get_sensor(RIGHT) > SENSOR_THRESHOLD);
+        while (black(get_sensor(RIGHT)));
         reverse();
-        while (get_sensor(RIGHT) < SENSOR_THRESHOLD);
+        while (white(get_sensor(RIGHT)));
     }
     _delay(250000);
 
     stop();
 }
 
+/* ================================
+Function: adjust_position
+Paramaters: none
+return type: none
+Description: Lines robot up to
+read barcode.
+================================ */
 void adjust_position(void)
 {
     forward();
@@ -373,7 +432,7 @@ void adjust_position(void)
         turn_right();
     }
 
-    while (get_sensor(RIGHT_SENSOR) < SENSOR_THRESHOLD && get_sensor(LEFT_SENSOR) < SENSOR_THRESHOLD)
+    while (white(get_sensor(RIGHT_SENSOR)) && white(get_sensor(LEFT_SENSOR)))
     {
         forward();
     }
@@ -408,6 +467,14 @@ void adjust_position(void)
     _delay(1000000);
 }
 
+/* ================================
+Function: enter
+Paramaters: enum Direction direction
+return type: none
+Description: Moves robot into
+a section either from the right or
+left depending on the argument given.
+================================ */
 void enter(enum Direction direction)
 {
     if (direction == RIGHT)
@@ -443,10 +510,21 @@ void enter(enum Direction direction)
     }
 }
 
+/* ================================
+Function: leave
+Paramaters: enum Direction direction
+return type: none
+Description: Moves robot out of
+starting section either right or
+left depending on the argument given.
+================================ */
 void leave(enum Direction direction)
 {
     forward();
-    while (!(black(right_sensor)) && black(get_sensor(left_sensor)))
+
+    right_sensor = get_sensor(RIGHT_SENSOR);
+    left_sensor = get_sensor(LEFT_SENSOR);
+    while (white(right_sensor) && white(left_sensor))
     {
         right_sensor = get_sensor(RIGHT_SENSOR);
         left_sensor = get_sensor(LEFT_SENSOR);
@@ -470,11 +548,18 @@ void leave(enum Direction direction)
     }
 }
 
+/* ================================
+Function: start
+Paramaters: none
+return type: none
+Description: Drives robot to the
+entrance of the barcode.
+================================ */
 void start(void)
 {
     forward();
-    while (white(get_sensor(LEFT_SENSOR)));
-    while (black(get_sensor(LEFT_SENSOR)));
+    while (white(get_sensor(RIGHT_SENSOR)));
+    while (black(get_sensor(RIGHT_SENSOR)));
 
     leave(RIGHT);
 
@@ -487,6 +572,14 @@ void start(void)
     }
 }
 
+/* ================================
+Function: counter_marker
+Paramaters: enum Direction direction
+return type: none
+Description: Counts the number of
+parking spots on the outside of the
+playing field.
+================================ */
 void count_marker(enum Direction direction)
 {
     if (direction == RIGHT)
@@ -511,10 +604,15 @@ void count_marker(enum Direction direction)
             }
         }
     }
-
-    PORTC = marker_count;
 }
 
+/* ================================
+Function: drive_left
+Paramaters: none
+return type: none
+Description: Makes the robot follow
+a line moving counterclockwise.
+================================ */
 void drive_left(void)
 {
     left_sensor = get_sensor(LEFT_SENSOR);
@@ -530,6 +628,13 @@ void drive_left(void)
     }
 }
 
+/* ================================
+Function: drive_right
+Paramaters: none
+return type: none
+Description: Makes the robot follow
+a line moving clockwise.
+================================ */
 void drive_right(void)
 {
     left_sensor = get_sensor(LEFT_SENSOR);
@@ -545,6 +650,13 @@ void drive_right(void)
     }
 }
 
+/* ================================
+Function: reverse_right
+Paramaters: none
+return type: none
+Description: Moves right motor
+backwards.
+================================ */
 void reverse_right(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -553,7 +665,13 @@ void reverse_right(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
-
+/* ================================
+Function: reverse_left
+Paramaters: none
+return type: none
+Description: Moves left motor
+backwards.
+================================ */
 void reverse_left(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -562,6 +680,13 @@ void reverse_left(void)
     LEFT_MOTOR_REVERSE = 1;
 }
 
+/* ================================
+Function: swing_right
+Paramaters: none
+return type: none
+Description: Moves left motor
+forward.
+================================ */
 void swing_right(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -570,6 +695,13 @@ void swing_right(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
+/* ================================
+Function: swing_left
+Paramaters: none
+return type: none
+Description: Moves right motor
+forward.
+================================ */
 void swing_left(void)
 {
     RIGHT_MOTOR_FORWARD = 1;
@@ -578,6 +710,13 @@ void swing_left(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
+/* ================================
+Function: turn_right
+Paramaters: none
+return type: none
+Description: Moves left motor
+forward and right motor backwards.
+================================ */
 void turn_right(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -586,6 +725,13 @@ void turn_right(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
+/* ================================
+Function: turn_left
+Paramaters: none
+return type: none
+Description: Moves right motor
+forward and left motor backwards.
+================================ */
 void turn_left(void)
 {
     RIGHT_MOTOR_FORWARD = 1;
@@ -594,6 +740,12 @@ void turn_left(void)
     LEFT_MOTOR_REVERSE = 1;
 }
 
+/* ================================
+Function: stop
+Paramaters: none
+return type: none
+Description: Stops both motors.
+================================ */
 void stop(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -602,6 +754,12 @@ void stop(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
+/* ================================
+Function: forward
+Paramaters: none
+return type: none
+Description: Moves motors forward.
+================================ */
 void forward(void)
 {
     RIGHT_MOTOR_FORWARD = 1;
@@ -610,6 +768,12 @@ void forward(void)
     LEFT_MOTOR_REVERSE = 0;
 }
 
+/* ================================
+Function: reverse
+Paramaters: none
+return type: none
+Description: Reverses both motors.
+================================ */
 void reverse(void)
 {
     RIGHT_MOTOR_FORWARD = 0;
@@ -618,11 +782,26 @@ void reverse(void)
     LEFT_MOTOR_REVERSE = 1;
 }
 
+/* ================================
+Function: test
+Paramaters: none
+return type: none
+Description: Test function for
+testing new code before implementing.
+================================ */
 void test(void)
 {
     // PUT TEST CODE HERE
 }
 
+/* ================================
+Function: reset_barcode_width
+Paramaters: none
+return type: none
+Description: Initialization function
+that clears the array that stores
+the width of the barcode lines.
+================================ */
 void reset_barcode_width(void)
 {
     for (int i = 0; i < 5; i++)
@@ -631,6 +810,13 @@ void reset_barcode_width(void)
     }
 }
 
+/* ================================
+Function: black
+Paramaters: int reading
+return type: char
+Description: Returns 1 if sensor
+reads black.
+================================ */
 char black(int reading)
 {
     if (reading > SENSOR_THRESHOLD)
@@ -643,6 +829,14 @@ char black(int reading)
     }
 }
 
+
+/* ================================
+Function: white
+Paramaters: int reading
+return type: char
+Description: Returns 1 if sensor
+reads white.
+================================ */
 char white(int reading)
 {
     if (reading < SENSOR_THRESHOLD)
@@ -655,6 +849,13 @@ char white(int reading)
     }
 }
 
+/* ================================
+Function: get_sensor
+Paramaters: enum Sensor side
+return type: int
+Description: Reads sensor of the
+side given and returns its value.
+================================ */
 int get_sensor(enum Sensor side)
 {
     if (side == RIGHT_SENSOR)
@@ -671,6 +872,14 @@ int get_sensor(enum Sensor side)
     return ADRESH;
 }
 
+
+/* ================================
+Function: init_hardware
+Paramaters: none
+return type: none
+Description: Initializes PIC
+hardware for the PIC Testboard.
+================================ */
 void init_hardware(void)
 {
 	TRISA = 0b00110000;
